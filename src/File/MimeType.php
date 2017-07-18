ThomasCantonnet/zend-validator

commit 3fc1e6614672eb7dbba816677c5a660a953bb563

Ref #7629
Closes opened file with finfo to avoid segfault on specific PHP versions

src/File/MimeType.php
@@ -385,6 +385,7 @@ class MimeType extends AbstractValidator
             $this->type = null;
             if (! empty($this->finfo)) {
                 $this->type = finfo_file($this->finfo, $file);
+                finfo_close($this->finfo);
             }
         }
 

commit 658e14ab3748c591f64bca6f8ceb3b20bcff5b0a

Fix BC break introduced by 3fc1e66

src/File/MimeType.php
@@ -385,7 +385,7 @@ class MimeType extends AbstractValidator
             $this->type = null;
             if (! empty($this->finfo)) {
                 $this->type = finfo_file($this->finfo, $file);
-                finfo_close($this->finfo);
+                unset($this->finfo);
             }
         }
 